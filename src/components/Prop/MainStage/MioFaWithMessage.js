ea7a51596528d5bfe6e41f81d6ee6acf59be5a64
Cuong Mai (R Jim) <vucuongJim@gmail.com>
2022-02-28 19:07:06 +0700

    Add message box shadow (#16)

--- src/components/Prop/MainStage/MioFaWithMessage.js
+++ src/components/Prop/MainStage/MioFaWithMessage.js
@@ -5,12 +5,17 @@ import './MioFaWithMessage.css'
 const message_border_colors = ["#e6e7e7", "#7b96d5", "#7b96d5", "#7b96d5", "#b20335"]
 
 class MioFaWithMessage extends Component {
+    getBorderColor = () => {
+        return message_border_colors[Math.floor(Math.random() * message_border_colors.length)]
+    }
+
     render() {
         const { message, isPreview } = this.props
-        const borderColor = message_border_colors[Math.floor(Math.random() * message_border_colors.length)]
+        const borderColor = this.getBorderColor()
+        const boxShadow = "0 0 12px " + borderColor
         return (
             <div className="miofa-with-message">
-                <div className={"message-container" + (message.length > 100 ? " big" : "")} style={{ borderColor }}>
+                <div className={"message-container" + (message.length > 100 ? " big" : "")} style={{ borderColor, boxShadow }}>
                     {message}
                 </div>
                 <MioFa disablePop={true} isPreview={isPreview} />
