de78a93865279eab316647b7fb672da462e1a160
unknown <vucuongJim@gmail.com>
2022-04-12 21:22:51 +0700

    format code

--- src/reducers/form.js
+++ src/reducers/form.js
@@ -1,8 +1,10 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import api from '../api'
 
-
-
+export const sendMessages = createAsyncThunk('posts/sendMessages', async ({ name, email, frames, message }) => {
+    const response = await api.sendMessage({ name, email, frames, message })
+    return response
+})
 
 const formSlice = createSlice({
     name: 'form',
